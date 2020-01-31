44dfffef0e59be00dbbded840f10480edd7f5d74
Nicolaas Nel <nicmeister@live.co.za>
2020-01-31 22:22:13 +0200

    Added newest article and multiple cards.

--- personal-blog/src/app/views/home/home.component.ts
+++ personal-blog/src/app/views/home/home.component.ts
@@ -33,6 +33,30 @@ export class HomeComponent implements OnInit {
       headline: "HALO TOURNAMENT",
       description: "Description",
       image: "https://content.halocdn.com/media/Default/Halo.gg/542x305_Module_Images/hcs-5ya-bde5e39fc4b141b1a423213bbefabe7f.png"
+    },
+    {
+      type: "TYPE",
+      headline: "HEADLINE",
+      description: "Description",
+      image: "https://glowvarietyshow.com/wp-content/uploads/2017/03/placeholder-image.jpg"
+    },
+    {
+      type: "TUTORIAL",
+      headline: "BUILD WEBSITE",
+      description: "Description",
+      image: "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/21_Angular_logo_logos-512.png"
+    },
+    {
+      type: "VIDEO",
+      headline: "NEW YOUTUBE VIDEO",
+      description: "Description",
+      image: "https://lh3.googleusercontent.com/z6Sl4j9zQ88oUKNy0G3PAMiVwy8DzQLh_ygyvBXv0zVNUZ_wQPN_n7EAR2By3dhoUpX7kTpaHjRPni1MHwKpaBJbpNqdEsHZsH4q"
+    },
+    {
+      type: "ANNOUNCEMENT",
+      headline: "HALO TOURNAMENT",
+      description: "Description",
+      image: "https://content.halocdn.com/media/Default/Halo.gg/542x305_Module_Images/hcs-5ya-bde5e39fc4b141b1a423213bbefabe7f.png"
     }
   ]
 
